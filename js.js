1c1be303bd1bd91cdf4b76c1870dade0dba6a237
Umberto Amoroso <34135219+Uesone@users.noreply.github.com>
2024-06-11 16:34:50 +0200

    JS
    JS FORM REGISTRATION

---
+++ js.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Recupera il nome salvato da localStorage
+  const userName = localStorage.getItem("username");
+  if (userName) {
+    document.getElementById("userName").innerText = "User: " + userName;
+  }
+
+  // Imposta il contatore
+  let counter = sessionStorage.getItem("counter");
+  if (!counter) {
+    counter = 0;
+  } else {
+    counter = parseInt(counter);
+  }
+
+  document.getElementById("counter").innerText = counter;
+
+  setInterval(() => {
+    counter++;
+    sessionStorage.setItem("counter", counter);
+    document.getElementById("counter").innerText = counter;
+  }, 1000);
+
+  // Gestione dei pulsanti
+  document.getElementById("saveButton").addEventListener("click", function () {
+    const username = document.getElementById("username").value;
+    localStorage.setItem("username", username);
+    document.getElementById("userName").innerText = "User salvato: " + username;
+  });
+
+  document
+    .getElementById("removeButton")
+    .addEventListener("click", function () {
+      localStorage.removeItem("username");
+      document.getElementById("userName").innerText = "";
+    });
+});
